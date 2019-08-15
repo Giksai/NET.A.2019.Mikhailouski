8f5ce249caae92a905c03257caa35f6eca810866
Giksai <vermione9@gmail.com>
2019-08-15 19:08:34 +0300

    Fourth day tasks completed

---
+++ NET.A.2019.Mikhailouski.04/FourthDayLib.Tests/UnitTest1.cs
@@ -0,0 +1,61 @@
+using NUnit.Framework;
+using FourthDayLibrary;
+namespace Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCase(1, 2, 3, 4, 5, 6, ExpectedResult = 1)]
+        [TestCase(35, 10, 15, ExpectedResult = 5)]
+        [TestCase(111, 33, 12, 48, ExpectedResult = 3)]
+        [TestCase(5, ExpectedResult = 5)]
+        [TestCase(5, 10, ExpectedResult = 5)]
+        [TestCase(0, ExpectedResult = null)]
+        [TestCase(null, ExpectedResult = null)]
+        public int EuclidGCDTests(params int[] input)
+        {
+            long timeElapsed;
+            return FourthDayLib.EuclidGCD(out timeElapsed, input);
+        }
+
+        [TestCase(1, 2, 3, 4, 5, 6, ExpectedResult = 1)]
+        [TestCase(35, 10, 15, ExpectedResult = 5)]
+        [TestCase(111, 33, 12, 48, ExpectedResult = 3)]
+        [TestCase(5, ExpectedResult = 5)]
+        [TestCase(5, 10, ExpectedResult = 5)]
+        [TestCase(0, ExpectedResult = null)]
+        [TestCase(null, ExpectedResult = null)]
+        public int SteinGCDTests(params int[] input)
+        {
+            long timeElapsed;
+            return FourthDayLib.SteinGCD(out timeElapsed, input);
+        }
+
+        [TestCase(-255.255, ExpectedResult =
+"1100000001101111111010000010100011110101110000101000111101011100")]
+        [TestCase(255.255, ExpectedResult =
+"0100000001101111111010000010100011110101110000101000111101011100")]
+        [TestCase(4294967295.0, ExpectedResult =
+"0100000111101111111111111111111111111111111000000000000000000000")]
+        [TestCase(double.MinValue, ExpectedResult =
+"1111111111101111111111111111111111111111111111111111111111111111")]
+        [TestCase(double.MaxValue, ExpectedResult =
+"0111111111101111111111111111111111111111111111111111111111111111")]
+        [TestCase(double.Epsilon, ExpectedResult =
+"0000000000000000000000000000000000000000000000000000000000000001")]
+        [TestCase(double.NaN, ExpectedResult =
+"1111111111111000000000000000000000000000000000000000000000000000")]
+        [TestCase(double.NegativeInfinity, ExpectedResult =
+"1111111111110000000000000000000000000000000000000000000000000000")]
+        [TestCase(double.PositiveInfinity, ExpectedResult =
+"0111111111110000000000000000000000000000000000000000000000000000")]
+        public string ToBinaryStringTests(double input)
+        {
+            return input.ToBinaryString();
+        }
+    }
+}
---
+++ NET.A.2019.Mikhailouski.04/FourthDayLibrary/FourthDayLib.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace FourthDayLibrary
+{
+    public static class FourthDayLib
+    {
+        #region FirstTask
+        private static void CheckArray(int[] input)
+        {
+            if (input == null) throw new ArgumentNullException();
+            if (input.Length == 0) throw new ArgumentException("Input array length is zero");
+            if (HasZerosInArray(input)) throw new ArgumentException("Having zeros in input array is forbidden");
+        }
+        public static int EuclidGCD(out long timeElapsed, params int[] input)
+        {
+            timeElapsed = 0;
+            Stopwatch stpWatch = new Stopwatch();
+            stpWatch.Start();
+
+            CheckArray(input);
+            if (input.Length == 1) return input[0];
+
+            while (!AllEqual(input))
+            {
+                input[FindMaxValueIndex(input)] -= input[FindMinValueIndex(input)];
+            }
+
+            timeElapsed = stpWatch.ElapsedMilliseconds;
+            stpWatch.Reset();
+
+            return input[0];
+        }
+
+        private static bool AllEqual(int[] input)
+        {
+            int comparer = input[0];
+            for (int i = 1; i < input.Length; i++)
+                if (input[i] != comparer) return false;
+
+            return true;
+        }
+        private static bool HasZerosInArray(int[] input)
+        {
+            foreach (var element in input)
+                if (element == 0) return true;
+
+            return false;
+        }
+        private static int FindMaxValueIndex(int[] input)
+        {
+            int max = 0;
+            foreach(var element in input)
+                if (element > max) max = element;
+
+            return Array.IndexOf(input, max);
+        }
+        private static int FindMinValueIndex(int[] input)
+        {
+            int min = int.MaxValue;
+            foreach (var element in input)
+                if (element < min) min = element;
+
+            return Array.IndexOf(input, min);
+        }
+
+        public static int SteinGCD(out long timeElapsed, params int[] input)
+        {
+            timeElapsed = 0;
+            Stopwatch stpWatch = new Stopwatch();
+            stpWatch.Start();
+
+            CheckArray(input);
+            if (input.Length == 1) return input[0];
+
+            int currentGCD = SteinGCDBasic(input[0], input[1]);
+
+            for (int i = 2; i < input.Length; i++)
+               currentGCD = SteinGCDBasic(currentGCD, input[i]);
+
+            timeElapsed = stpWatch.ElapsedMilliseconds;
+            stpWatch.Reset();
+
+            return currentGCD;
+        }
+        private static int SteinGCDBasic(int a, int b)
+        {
+            if (a == 0) return b;
+            if (b == 0) return a;
+            if (a == b) return a;
+            if (a == 1 || b == 1)  return 1;
+
+            if ((a & 1) == 0) 
+                return ((b & 1) == 0) ? SteinGCDBasic(a >> 1, b >> 1) << 1 : SteinGCDBasic(a >> 1, b);
+            else 
+                return ((b & 1) == 0) ? SteinGCDBasic(a, b >> 1) : SteinGCDBasic(b, a > b ? a - b : b - a);
+        }
+        #endregion
+
+        #region SecondTask
+        public static string ToBinaryString(this double input)
+        {
+            DoubleToLongStruct convertStruct = new DoubleToLongStruct { Double64bits = input };
+            long value = convertStruct.Long64bits;
+            int bitsCount = 64;
+            char[] result = new char[bitsCount];
+            result[0] = value < 0 ? '1' : '0';
+            for (int i = bitsCount - 2, j = 1; i >= 0; i--, j++)
+            {
+                result[j] = (value & (1L << i)) != 0 ? '1' : '0';
+            } 
+
+            return new string(result);
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        private struct DoubleToLongStruct
+        {
+            [FieldOffset(0)]
+            private readonly long long64bits;
+
+            [FieldOffset(0)]
+            private double double64bits;
+
+            public double Double64bits
+            {
+                set
+                {
+                    double64bits = value;
+                }
+            }
+
+            public long Long64bits
+            {
+                get
+                {
+                    return long64bits;
+                }
+            }
+        }
+        #endregion
+    }
+}
